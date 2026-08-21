a4fa47be5394050fd90e20e0482b8299f1be38ba
TeslaWillow <hectorjosue.caballero@gmail.com>
2026-08-20 18:15:42 -0600

    feat: config for npm

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { AudioVisualizer } from "./components/AudioVisualizer";
+export { AudioProcessor } from "./core/AudioProcessor";
+export { useAudioAnalyzer } from "./hooks/useAudioAnalyzer";
+
+export { BaseRenderer } from "./renderers/base/BaseRenderer";
+export { BarsWaveRenderer } from "./renderers/canvas/BarsWaveRenderer";
+export { CircularWaveRenderer } from "./renderers/canvas/CircularWaveRenderer";
+export { TunnelWaveRenderer } from "./renderers/webgl/TunnelWaveRenderer";
+
+export * from "./types/visualizer";
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+// ./vite.config.ts
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(import.meta.dirname, "src/index.ts"),
+      name: "AudioVisualizer",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
